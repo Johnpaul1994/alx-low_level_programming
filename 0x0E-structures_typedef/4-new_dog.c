1d20cab437f45f81f09dc3a8b0b2a8e9c8342ce0
Johnpaul1994 <johnpaulnedolisa@gmail.com>
2022-10-11 11:39:06 +0100

    new one

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,49 +1,55 @@
-#include "dog.h"
+#include <stdio.h>
 #include <stdlib.h>
-
+#include "dog.h"
 /**
- * new_dog - create a new dog
- * @name: char string name
- * @age: int age
- * @owner: char string owner
- * Return: pointer to new dog
+ * new_dog - creates a new dog
+ *
+ * @name: pointer to a variable to copy
+ * @owner: pointer to a variable to copy
+ * @age: float variable type
+ *
+ * Return: to dog_t or NULL if it fail
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	struct dog *doge;
-	int i, j, k;
-	char *n, *o;
+	/* dog_t is the new name of dog */
+	/* two variables to know the size of the variables I need to copy */
+	int namerunner, ownerrunner;
+	/* allocate the *new_dog function and test if is null or not */
+	dog_t *new_dog = malloc(sizeof(dog_t));
 
-	doge = malloc(sizeof(struct dog));
-	if (doge == NULL)
-		return (NULL);
-
-	for (i = 0; name[i] != '\0'; i++)
-		;
-	for (j = 0; owner[j] != '\0'; j++)
-		;
-
-	n = malloc(sizeof(char) * i + 1);
-if (n == NULL)
+	if (new_dog == NULL)
+		return (new_dog);
+	/* time to know the size of the *name and *owner */
+	for (namerunner = 0; name[namerunner]; namerunner++)
 	{
-		free(doge); 
+	}
+	for (ownerrunner = 0; owner[ownerrunner]; ownerrunner++)
+	{
+	}
+	/* Add in new_dog the size of the variables name and owner */
+	/* dereference new_dog to each of the variables */
+	/* plus 1 to the runners because I need to save space for \0 too */
+	new_dog->name = malloc((namerunner + 1) * sizeof(char));
+	new_dog->owner = malloc((ownerrunner + 1) * sizeof(char));
+	/*check allocation was right if NULL free all the allocated memory*/
+	/* we freed memory from what is inside to the big function new_dog */
+	if (new_dog->name == NULL || new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog->owner);
+		free(new_dog);
 		return (NULL);
 	}
-	o = malloc(sizeof(char) * j + 1);
-	if (o == NULL)
-	{
-		free(n);
-		free(doge);
-		return (NULL);
-	}
-	for (k = 0; k <= i; k++)
-		n[k] = name[k];
-	for (k = 0; k <= j; k++)
-		o[k] = owner[k];
-
-	doge->name = n;
-	doge->age = age;
-	doge->owner = o;
-
-	return (doge);
+	/* once I have the memory allocated */
+	/* I start copying the variables to the malloc pointer *new_dog */
+	for (namerunner = 0; name[namerunner]; namerunner++)
+		new_dog->name[namerunner] = name[namerunner];
+	for (ownerrunner = 0; owner[ownerrunner]; ownerrunner++)
+		new_dog->owner[ownerrunner] = owner[ownerrunner];
+	/* now I set them to zero so the function return the output I need */
+	new_dog->name[namerunner] = 0;
+	new_dog->owner[ownerrunner] = 0;
+	new_dog->age = age;
+	return (new_dog);
 }
